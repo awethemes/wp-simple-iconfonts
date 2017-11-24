f89b3c5911f052eaf625bfe6ab8d0552d751b09d
Nguyen Van Anh <anhskohbo@gmail.com>
2017-11-24 11:45:31 +0700

    Code format

--- inc/Support/Shortcode_Icon.php
+++ inc/Support/Shortcode_Icon.php
@@ -26,18 +26,22 @@ class Shortcode_Icon {
 	 * Constructor.
 	 */
 	private function __construct() {
+		add_shortcode( 'wp_simple_iconfonts', array( $this, 'output_shortcode' ) );
+
+		add_action( 'wp_enqueue_editor', array( $this, 'wp_enqueue_editor' ) );
 		add_filter( 'mce_external_plugins', array( $this, 'enqueue_editor_scripts' ) );
 		add_filter( 'mce_buttons', array( $this, 'register_buttons_editor' ) );
-		add_action( 'wp_enqueue_editor', array( $this, 'wp_enqueue_editor' ) );
-		add_action( 'print_media_templates', array( $this, '_media_templates' ) );
-		add_shortcode( 'wp_simple_iconfonts', array( $this, 'add_editor_shortcode' ) );
+		add_action( 'print_media_templates', array( $this, 'print_media_templates' ) );
 	}
 
 	/**
 	 * Enqueue scripts.
+	 *
+	 * @access private
 	 */
 	public function wp_enqueue_editor() {
 		wp_enqueue_media();
+
 		wp_enqueue_style( 'wp-color-picker' );
 		wp_enqueue_script( 'wp-color-picker' );
 		wp_enqueue_style( 'simple-iconfonts-picker' );
@@ -51,12 +55,14 @@ class Shortcode_Icon {
 	/**
 	 * Enqueue editor scripts.
 	 *
-	 * @param  array $plugin_array plugin_array
+	 * @param  array $plugin_array The plugin array.
 	 * @return array
+	 *
+	 * @access private
 	 */
 	public function enqueue_editor_scripts( $plugin_array ) {
 		// Enqueue TinyMCE plugin script with its ID.
-		$plugin_array['wp_simple_icons_button'] = plugin_dir_url( WP_SIMPLE_ICONFONTS_PATH ) . '/js/simple-iconfonts-editor.js';
+		$plugin_array['wp_simple_icons_button'] = wp_simple_iconfonts()->get_plugin_url( 'js/simple-iconfonts-editor.js' );
 
 		return $plugin_array;
 	}
@@ -66,19 +72,23 @@ class Shortcode_Icon {
 	 *
 	 * @param  array $buttons buttons.
 	 * @return array
+	 *
+	 * @access private
 	 */
 	public function register_buttons_editor( $buttons ) {
 		// Register buttons with their id.
 		array_push( $buttons, 'wp_simple_icons_button' );
+
 		return $buttons;
 	}
 
 	/**
 	 * Get and print media templates from all types
 	 *
+	 * @access private
 	 */
-	public function _media_templates() {
-		include plugin_dir_path( __DIR__ ) . 'views/shortcode-media-template.php';
+	public function print_media_templates() {
+		include trailingslashit( __DIR__ ) . '/../views/shortcode-media-template.php';
 	}
 
 	/**
@@ -86,7 +96,7 @@ class Shortcode_Icon {
 	 *
 	 * @param array $atts atts.
 	 */
-	public function add_editor_shortcode( $atts ) {
+	public function output_shortcode( $atts ) {
 		$atts = shortcode_atts( array(
 			'type'        => '',
 			'icon'        => '',
--- js/simple-iconfonts-editor.js
+++ js/simple-iconfonts-editor.js
@@ -1,82 +1,79 @@
 (function($) {
-    'use strict';
+	'use strict';
 
-    var IconPicker = wp.media.controller.SimpleIconPicker;
+	var IconPicker = wp.media.controller.SimpleIconPicker;
 
-    var WP_SimpleIconFonts_Sidebar = wp.media.view.IconPickerSidebar.extend({
-        initialize: function() {
-            wp.media.view.IconPickerSidebar.prototype.initialize.apply( this, arguments );
+	var WP_SimpleIconFonts_Sidebar = wp.media.view.IconPickerSidebar.extend({
+		initialize: function() {
+			wp.media.view.IconPickerSidebar.prototype.initialize.apply(this, arguments);
 
-            this.$el.text('Setting');
+			this.$el.html($('#wp_simple_iconfonts_shortcode-template').html());
 
-            this.$el.html(
-                $('#wp_simple_iconfonts_shortcode-template').html()
-            );
+			// Setup some plugins.
+			$('#wp_simple_iconfonts_color', this.$el).wpColorPicker();
+		},
 
-            $("#wp_simple_iconfonts_color", this.$el).wpColorPicker();
-        },
+		createSingle: function() {
+		},
 
-        createSingle: function() {
-        },
+		disposeSingle: function() {
+		},
+	});
 
-        disposeSingle: function() {
-        },
-    });
+	tinymce.create("tinymce.plugins.wp_simple_icons_button", {
+		frame: null,
 
-    tinymce.create("tinymce.plugins.wp_simple_icons_button", {
-        frame: null,
+		init: function(editor, url) {
+			this.frame = new IconPicker({
+				SidebarView: WP_SimpleIconFonts_Sidebar
+			});
 
-        init: function(editor, url) {
-            this.frame = new IconPicker({
-                SidebarView: WP_SimpleIconFonts_Sidebar
-            });
+			this.frame.target.on('change', this.handleInsertShortcode.bind(this, editor));
 
-            this.frame.target.on('change', this.handleInsertShortcode.bind(this, editor));
+			editor.addButton('wp_simple_icons_button', {
+				title : 'WP Simple Iconfont',
+				icon: 'icon dashicons-star-empty',
+				onclick: this.handleOnClick.bind(this),
+			});
+		},
 
-            editor.addButton("wp_simple_icons_button", {
-                title : "WP Simple Iconfont",
-                icon: 'icon dashicons-star-empty',
-                onclick: this.handleOnClick.bind(this),
-            });
-        },
+		handleOnClick: function(e) {
+			this.getFrame().open();
+		},
 
-        handleOnClick: function(e) {
-            this.getFrame().open();
-        },
+		handleInsertShortcode: function(editor, model) {
+			var icon_atts = model.attributes;
+			if ( ! icon_atts ) {
+				return;
+			}
 
-        handleInsertShortcode: function(editor, model) {
-            var icon_atts = model.attributes;
-            if ( ! icon_atts ) {
-                return;
-            }
+			var type = icon_atts.type,
+				icon = icon_atts.icon,
+				url  = icon_atts.url;
 
-            var type  = icon_atts.type,
-                icon  = icon_atts.icon,
-                url   = icon_atts.url;
+			// Build shortcode attributes.
+			var $attributes = {
+				type: type,
+				icon: icon,
+				url: url,
+				color: $('#wp_simple_iconfonts_color').val(),
+				font_size: $('#wp_simple_iconfonts_font_size').val(),
+				font_weight: $('#wp_simple_iconfonts_font_weight').val(),
+			};
 
-            // Build shortcode attributes.
-            var $attributes = {
-              "type": type,
-              "icon": icon,
-              "url": url,
-              "font_size": $('#wp_simple_iconfonts_font_size').val(),
-              "font_weight": $('#wp_simple_iconfonts_font_weight').val(),
-              "color": $('#wp_simple_iconfonts_color').val(),
-            };
+			var $html_attributes = '';
+			$.each( $attributes, function(key, value) {
+				$html_attributes += key + '="' + value + '" ';
+			});
 
-            var $html_attributes = '';
-            $.each( $attributes, function( key, value ) {
-              $html_attributes += key + '="' + value + '" ';
-            });             
-            
-            editor.insertContent( '[wp_simple_iconfonts ' + $html_attributes +']' );
-        },
+			editor.insertContent('[wp_simple_iconfonts ' + $html_attributes +']');
+		},
 
-        getFrame: function() {
-            return this.frame;
-        },
-    });
+		getFrame: function() {
+			return this.frame;
+		},
+	});
 
-    tinymce.PluginManager.add("wp_simple_icons_button", tinymce.plugins.wp_simple_icons_button);
+	tinymce.PluginManager.add('wp_simple_icons_button', tinymce.plugins.wp_simple_icons_button);
 
 })(jQuery);
